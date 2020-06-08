265112e8b6bf16d72d7c87c468e5092e3a56e7c1
MachTheory <dwhitehead90@yahoo.com>
2020-06-08 15:28:58 -0400

    FAB and alert dialog added

--- app/src/main/java/com/machtheory/todoo/MainActivity.java
+++ app/src/main/java/com/machtheory/todoo/MainActivity.java
@@ -2,14 +2,19 @@ package com.machtheory.todoo;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.tabs.TabLayout;
 
 /* This Program is a Kanban Style To Do List to keep users' organized*/
@@ -18,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
 
     TabLayout tabLayout;
     ViewPager viewPager;
+    FloatingActionButton addButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,10 +32,26 @@ public class MainActivity extends AppCompatActivity {
 
         tabLayout = (TabLayout)findViewById(R.id.tabLayout);
         viewPager = (ViewPager)findViewById(R.id.viewPager);
+        addButton = (FloatingActionButton)findViewById(R.id.addButton);
 
         viewPager.setAdapter(new ViewPagerAdapter(getSupportFragmentManager()));
         tabLayout.setupWithViewPager(viewPager);
 
+    }
+
+    public void setAddButton(View view){
+        Toast.makeText(this, "Button Selected", Toast.LENGTH_SHORT).show();
+        new AlertDialog.Builder(this).setIcon(android.R.drawable.btn_plus)
+                .setTitle("Add new item")
+                .setMessage("enter item below")
+                .setPositiveButton("Add to To Do", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Toast.makeText(MainActivity.this, "add to do selected", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .setNegativeButton("Cancel", null)
+                .show();
     }
 
     class ViewPagerAdapter extends FragmentPagerAdapter{
